b11d90547b0775d22a49bf027470061c0e379742
L9L4 <lorenzo.lastilla@uniroma1.it>
2020-07-03 10:23:27 +0200

    Pose mirroring for method 1

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import math
+import copy
+from copy import deepcopy
 import numpy as np
 
 def ang(v):
@@ -73,3 +75,25 @@ def dist_medie(dict_joints):
 
     dist_medie = np.mean(dist_joints,0)
     return 100*dist_medie
+
+def pose_mirroring(pose):
+    new_order = [0, 1, 5, 6, 7, 2, 3, 4, 8, 12, 13, 14, 9, 10,  11]
+    new_pose = [pose[i] for i in new_order]
+    M = max([new_pose[n][0] for n in range(len(new_pose))])
+
+    mirrored_pose = copy.deepcopy(new_pose)
+
+    for i in range(len(mirrored_pose)):
+      mirrored_pose[i][0] = - new_pose[i][0] + M
+
+    return mirrored_pose
+
+def turn_pose(pose):
+    M = max([pose[n][0] for n in range(len(pose))])
+
+    t_pose = copy.deepcopy(pose)
+
+    for i in range(len(t_pose)):
+      t_pose[i][0] = - pose[i][0] + M
+
+    return t_pose
